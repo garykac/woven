d5e8a0a5e527b611f0b6152319545b9856a84d4c
Gary Kacmarcik <garykac@gmail.com>
2020-07-12 08:13:26 -0700

    Add note about swapping backs of sz4 and t4 tapestry cards

--- components/data_tapestry_cards.py
+++ components/data_tapestry_cards.py
@@ -33,26 +33,6 @@ tapestry_card_data = [
     },
 
     # +---------+       +---------+
-    # |    X  X |  <->  | X  X    |
-    # | X  X    |       |    X  X |
-    # +---------+       +---------+
-    {   'name': 'sz4',
-        'pattern': ['1 X X',  'X X 4',
-                    'X X 2',  '3 X X'],
-        # 1: A A W F F E  =  aa ff e w  =  2-af ew
-        # 2: F E A E W W  =  a f ee ww  =  af 2-ew
-        # 3: W F F A E A  =  aa ff e a  =  2-af ew
-        # 4: E W E W A F  =  a f ee ww  =  af 2-ew
-        #
-        # X X
-        # X .  =  aaa fff eee www
-        #
-        # X X
-        # . X  =  aaa fff eee www
-        'elements': ['afwe', 'aefw', 'wafe', 'feaw', 'fwea', 'ewaf'],
-    },
-
-    # +---------+       +---------+
     # | X  X  X |  <->  | X  X  X |
     # | X       |       |       X |
     # +---------+       +---------+
@@ -89,6 +69,30 @@ tapestry_card_data = [
         #
         # X X
         # . X  =  aa ff e w
+        'elements': ['afwe', 'aefw', 'wafe', 'feaw', 'fwea', 'ewaf'],
+    },
+
+    # TODO: Consider swapping backs between sz4 and t4.
+    # This will make more patterns castable on all cards.
+    # https://github.com/garykac/woven/issues/19
+    
+    # +---------+       +---------+
+    # |    X  X |  <->  | X  X    |
+    # | X  X    |       |    X  X |
+    # +---------+       +---------+
+    {   'name': 'sz4',
+        'pattern': ['1 X X',  'X X 4',
+                    'X X 2',  '3 X X'],
+        # 1: A A W F F E  =  aa ff e w  =  2-af ew
+        # 2: F E A E W W  =  a f ee ww  =  af 2-ew
+        # 3: W F F A E A  =  aa ff e a  =  2-af ew
+        # 4: E W E W A F  =  a f ee ww  =  af 2-ew
+        #
+        # X X
+        # X .  =  aaa fff eee www
+        #
+        # X X
+        # . X  =  aaa fff eee www
         'elements': ['afwe', 'aefw', 'wafe', 'feaw', 'fwea', 'ewaf'],
     },
 
