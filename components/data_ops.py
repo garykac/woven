8a7ef4336f1ae276fb516aa548ae18a1b118ae03
Gary Kacmarcik <garykac@gmail.com>
2020-09-27 14:29:43 -0700

    Disable unused ops

--- components/data_ops.py
+++ components/data_ops.py
@@ -2,26 +2,26 @@
 valid_ops = [
     'tapestry',       # Draw tapestry card
     'tapestry-eye',   # Draw tapestry card OR Create eye
-    'tapestry-emove', # Draw tapestry card OR Move eye
+    #'tapestry-emove', # Draw tapestry card OR Move eye
     'tapestry-mmove', # Draw tapestry card OR Move mage
     'tapestry-thread',# Draw tapestry card OR Recover thread
-    'tapestry-tmove', # Draw tapestry card OR Move thread
+    #'tapestry-tmove', # Draw tapestry card OR Move thread
     'eye',            # Create eye
-    'eye-emove',      # Create eye OR Move eye
+    #'eye-emove',      # Create eye OR Move eye
     'eye-mmove',      # Create eye OR Move mage
     'eye-thread',     # Create eye OR Recover thread
-    'eye-tmove',      # Create eye OR Move thread
-    'emove',          # Move eye
-    'emove-mmove',    # Move eye OR Move mage
-    'emove-thread',   # Move eye OR Recover thread
-    'emove-tmove',    # Move eye OR Move thread
+    #'eye-tmove',      # Create eye OR Move thread
+    #'emove',          # Move eye
+    #'emove-mmove',    # Move eye OR Move mage
+    #'emove-thread',   # Move eye OR Recover thread
+    #'emove-tmove',    # Move eye OR Move thread
     'mmove',          # Move mage
     'mmove-thread',   # Move mage OR Recover thread
-    'mmove-tmove',    # Move mage OR Move thread
+    #'mmove-tmove',    # Move mage OR Move thread
     'thread',         # Recover thread
-    'thread-tmove',   # Recover thread OR Move thread
-    'tmove',          # Move thread
-    'tmove-action',   # Move thread AND take another action
+    #'thread-tmove',   # Recover thread OR Move thread
+    #'tmove',          # Move thread
+    #'tmove-action',   # Move thread AND take another action
     'action',         # Take another action
     'action-action',  # Take another 2 actions
 ]
