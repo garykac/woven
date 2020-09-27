1777fc0b1b10dc3e5aae0e5b0ec6f2bf2074ec5e
Gary Kacmarcik <garykac@gmail.com>
2020-09-27 05:42:06 -0700

    Update Artifact cards

--- components/data_artifact_cards.py
+++ components/data_artifact_cards.py
@@ -3,11 +3,194 @@
 
 artifact_card_data = [
 
-    ["Nose Ring of Mental Acuity",
-        {'op': 'tmove-action', 'vp': 3,
+    ["Amulet of Mental Acuity",
+        {'op': 'action-action', 'vp': 1,
+         'description': "",
+         'bonus': "+2 for each set of 2 adjacent Water in Tapestry",
+         'flavor': "",
         } ],
 
-    ["Visbeth's Glass Eye",
-        {'op': 'action-action', 'vp': 3,
+    ["Visbeth's Eye",
+        {'op': 'action-action', 'vp': 1,
+         'description': "",
+         'bonus': "+2 for every set of 4 elements in Tapestry",
+         'flavor': "Always watching",
         } ],
+
+    ["Grizelia's Left Boot of Binding",
+        {'op': 'action-action', 'vp': 1,
+         'description': "",
+         'bonus': "+1 VP for each Earth spell",
+         'flavor': "",
+        } ],
+
+    ["Mirror of the Faceless Abyss",
+        {'op': 'action-action', 'vp': 3,
+         'description': "",
+         'bonus': "",
+         'flavor': "",
+        } ],
+
+    ["Ring of the Infernal Queen",
+        {'op': 'action-action', 'vp': 3,
+         'description': "",
+         'bonus': "",
+         'flavor': "",
+        } ],
+
+    ["Blade of the Dark Flame",
+        {'op': 'action-action', 'vp': 1,
+         'description': "Move an Eye 5 spaces",
+         'bonus': "",
+         #'bonus': "If you successfully attack another player while The Blade is active, you win instantly.",
+         'flavor': "",
+        } ],
+
+    ["Book of Divine Contemplation",
+        {'op': 'action-action', 'vp': 3,
+         'description': "",
+         'bonus': "",
+         'flavor': "",
+        } ],
+
+    ["Chimera Mask",
+        {'op': 'action-action', 'vp': 3,
+         'description': "",
+         'bonus': "",
+         'flavor': "",
+        } ],
+
+    ["Orb of Midnight Neverending",
+        {'op': 'action-action', 'vp': 3,
+         'description': "",
+         'bonus': "",
+         'flavor': "",
+        } ],
+
+    ["Glowspider Necklace",
+        {'op': 'action-action', 'vp': 3,
+         'description': "",
+         'bonus': "",
+         'flavor': "",
+        } ],
+
+    ["Key of the Marked Guardian",
+        {'op': 'action-action', 'vp': 3,
+         'description': "",
+         'bonus': "",
+         'flavor': "",
+        } ],
+
+    ["Lamp of Endless Dawn",
+        {'op': 'action-action', 'vp': 3,
+         'description': "",
+         'bonus': "",
+         'flavor': "",
+        } ],
+
+    ["Cloak of the Watcher",
+        {'op': 'action-action', 'vp': 3,
+         'description': "",
+         'bonus': "",
+         'flavor': "",
+        } ],
+
+    ["The Elder's Heart",
+        {'op': 'action-action', 'vp': 3,
+         'description': "",
+         'bonus': "",
+         'flavor': "",
+        } ],
+
+    ["Cloak of Concealment",
+        {'op': 'action-action', 'vp': 2,
+         'description': [
+             "While active, you cannot be detected by creatures.",
+             "You may place a Charge on this Artifact to maintain its effect.",
+         ],
+         'bonus': "",
+         'flavor': "Protection from Detection",
+        } ],
+
+    # Special Abilities:
+    # Hold more cards in hand. Draw more cards into hand.
+
+    
+    # Items:
+    # Amulet, Ark, Armor, Arrow, Axe
+    # Blade, Bolt, Book, Boots, Bottle, Bow,
+    #   Bracelet, Bracer, Brooch, Broom, Buckler
+    # Cap, Carapace, Chalice, Charm, Claw, Cleaver, Cloak, Club,
+    #   Codex, Collar, Concoction, Coronet, Crown, Crossbow, Cup
+    # Dagger, Draught
+    # Egg, Elixir, Essence, Eye
+    # Fang, Flask, Flute
+    # Gauntlet, Glaive, Globe, Glove, Grimoire
+    # Hammer, Hand, Heart, Helm, Helmet, Horn, Hourglass
+    # Idol
+    # Jar
+    # Key
+    # Lamp, Lance, Leviathan, Locket, Longbow
+    # Mace, Manual, Mask, Medallion, Mirror
+    # Necklace, Needle
+    # Obelisk, Ointment, Orb
+    # Paw, Pendant, Potion, Prism
+    # Quiver
+    # Ring, Robe, Rod, Rope
+    # Sarcophagus, Scale, Sceptor, Scroll, Scythe,
+    #   Shade, Shard, Shield, Sickle, Sigil, Skull,
+    #   Spark, Spear, Sphere, Staff, Statue, Stone, Sword
+    # Talisman, Tear, Thorn, Tiara, Token, Tome, Tonic
+    # Urn
+    # Veil, Vestments
+    # Wand, Ward, Whip
+
+    # Adj:
+    # Abyss, Absorption, Adamantine, Ancient,
+    #   Annihiliation, Anti, Arcane, Arch, Autumn
+    # Balanced, Binding, Black, Blessed, Blood, Blur,
+    #   Bond, Boon, Bottled, Brass, Brew, Burrowing
+    # Chain, Champion, Chimera, Clear, Cobalt,
+    #   Command, Concealing, Control, Cunning, Cursed
+    # Damned, Dark, Dawn, Death, Defender, Devil, Dimension,
+    #   Discord, Disguise, Dispel, Divine, Doom, Dragon, Dread
+    # Ebony, Elder, Eldritch, Elemental,
+    #   Endless, Endurance, Energy, Entanglement, Entrapment, Eternal,
+    #   Ever, Exalted, Excavation, Expedient, Expeditious, Explosive
+    # Faceless, Faerie, Fairy, Famine, Fey, Fidelity, Fiend, Fire, First,
+    #   Flame, Flare, Fortitude, Frost, Fury
+    # Gentle, Giant, Glass, Glowing, Gold, Great, Guardian
+    # Hallowed, Healing, Hell, High, Holy, Horns, Howling
+    # Ice, Illusion, Incarnate, Infection,
+    #   Infernal, Infinite, Intuition, Invulnerable, Iron
+    # King
+    # Lash, Last, Light, Lightning, Lord, Luck, Lunar
+    # Magi, Major, Mark, Meld, Midnight, Might, Mind,
+    #   Minor, Moon, Moonlight, Morph, Mutate
+    # Necromancer, Never-ending, Night
+    # Ocean, Omniscience, Order
+    # Peace, Perpetual, Plentiful, Portal,
+    #   Powdered, Power, Prismatic, Psychic, Pure
+    # Queen
+    # Razor, Recall, Refreshing, Rejuvenation,
+    #   Removal, Rend, Replenishment, Retribution, Ripper, Ruby
+    # Scorpion, Secret, Seeker, Seering, Shadow, Shatter, Shielding,
+    #   Silver, Slayer, Sleep, Song, Sorrow, Soul, Spawn,
+    #   Spectral, Spider, Spirit, Splendor, Spring, Stability, Star,
+    #   Starlight, Steal, Stoic, Stoneskin, Storm, Strength, Summer, Sun
+    # Thunder, Time, True
+    # Umber, Unbreakable, Unending, Unraveling, Unrelenting
+    # Vanguard, Vigor, Vile, Vision, Vitriolic, Void
+    # Weeping, Whisper, Winter, Wisdom, Witch, Wrath, Wretched
+    
+    # Animals/Monsters:
+    # Bear, Bull
+    # Cat, Crow
+    # Eagle
+    # Fox
+    # Goblin
+    # Owl
+    # Rat, Raven
+    # Troll
 ]
+
