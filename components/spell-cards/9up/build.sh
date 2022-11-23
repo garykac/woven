9ee506876f378e8c2b7f34a00ba9fc234eeca842
Gary Kacmarcik <garykac@gmail.com>
2022-11-23 08:18:28 -0800

    Generate 9-up PDFs

---
+++ components/spell-cards/9up/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# This script must be run from the spell-cards/9up directory.
+
+INKSCAPE="/Applications/Inkscape.app/Contents/MacOS/inkscape"
+
+for i in $(seq -f "%02g" 1 4)
+do
+	$INKSCAPE --export-filename pdf/9up-page$i.pdf --export-dpi=300 --export-text-to-path --export-area-page svg/9up-page$i.svg
+done
