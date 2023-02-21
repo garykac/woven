3d09b04263497cb2305e539ef122b3dc465c36ea
Gary Kacmarcik <garykac@gmail.com>
2023-02-20 21:16:14 -0800

    [Spell] Spell card 9up pdfs

--- components/spell-cards/9up/build.sh
+++ components/spell-cards/9up/build.sh
@@ -3,8 +3,12 @@
 
 INKSCAPE="/Applications/Inkscape.app/Contents/MacOS/inkscape"
 
-for i in $(seq -f "%02g" 1 4)
+for i in $(seq -f "%02g" 1 5)
 do
 	echo Generating spell PDF page$i 
 	$INKSCAPE --export-filename pdf/9up-page$i.pdf --export-dpi=300 --export-text-to-path --export-area-page svg/9up-page$i.svg
 done
+
+# Generate combined PDF.
+echo Combining PDFs
+gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=spells.pdf $(seq -f "pdf/9up-page%02g.pdf" 1 5)
